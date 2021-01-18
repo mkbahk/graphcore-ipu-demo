c03b3f2295800c2aa00055fc689b39cb1ced07e8
박문기 <mkbahk@gmail.com>
2021-01-18 13:21:01 +0900

    input and output added

---
+++ mnist-dnn-keras-functional-cpu-input-and-output.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+from tensorflow import keras
+import time
+import os
+
+start = time.time() # 시작 시간 저장
+strategy = tf.distribute.get_strategy()
+
+# The input data and labels.
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, x_test) = (x_train / 255.0, x_test / 255.0)
+
+# Add a channels dimension.
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
+def create_train_dataset():
+    print("==============================Processing Training DataSet==============================\n\n")
+    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(60000).batch(32, drop_remainder=True)
+    train_ds = train_ds.map(lambda d, l: (tf.cast(d, tf.float32), tf.cast(l, tf.float32)))
+    print(train_ds)
+    return train_ds.repeat()
+#end of def
+
+def create_test_dataset():
+    print("==============================Processing Test  DataSet==============================\n\n")
+    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).shuffle(10000).batch(32, drop_remainder=True)
+    test_ds = test_ds.map(lambda d, l: (tf.cast(d, tf.float32), tf.cast(l, tf.float32)))
+    return test_ds.repeat()
+#end of def
+
+def create_model(width=28, height=28, depth=1):
+    inputs = tf.keras.Input(shape=(width, height, depth))
+
+    x = keras.layers.Dense(128, activation='relu')(inputs)
+    x = keras.layers.Dense(256, activation='relu')(x)
+    x = keras.layers.Dense(128, activation='relu')(x)
+
+    outputs = keras.layers.Dense(10, activation='softmax')(x)
+    
+    # Defined the model.
+    model = tf.keras.Model(inputs, outputs, name="dnn")
+    return model
+#end of def
+
+def main():
+      # Get the training dataset.
+      print("==============================Getting Training DataSet==============================\n\n")
+      ds1 = create_train_dataset()
+      print("==============================Getting Test DataSet==============================\n\n")
+      ds2 = create_test_dataset()
+
+      with strategy.scope():   
+        # Create an instance of the model.
+        print("==============================Building Model==============================\n\n")
+        model = create_model()
+
+        model.summary()
+
+        print("==============================Building Compile==============================\n\n")      
+        model.compile(loss = tf.keras.losses.SparseCategoricalCrossentropy(), 
+                  optimizer = tf.keras.optimizers.Adam(), 
+                  steps_per_execution = 50, 
+                  metrics=['sparse_categorical_accuracy'])
+        
+        print("==============================Model Training ==============================\n\n")
+        model.fit(ds1, steps_per_epoch=2000, epochs=50)
+
+        print("\n\n==============================Checking the result==============================\n\n")
+        loss, accuracy = model.evaluate(ds2, steps=1000)
+        print("Validation loss: {}".format(loss))
+        print("Validation accuracy: {}%".format(100.0 * accuracy))
+        print("\n\n==============================Job Done==============================")
+        #end of with
+#end of def
+
+if __name__ == '__main__':
+    main()
+#end of if
+
+
+
+
