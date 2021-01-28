1afd9706329cd2d58c1d68ff1bfd82c8264dec25
Bahk, Moon-Kee <mkbahk@gmail.com>
2021-01-28 18:56:37 +0900

    Hello World

--- mnist-dnn-keras-sequential-cpu-basic.py
+++ mnist-dnn-keras-sequential-cpu-basic.py
@@ -2,11 +2,10 @@
 from tensorflow import keras
 import time
 
-
-# Load MNIST DataSet
+# Load MNIST DataSet from Internet somewhere
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
-# one-hot enconding 수행
+# INPU을 행렬방식으로 입력하기 위해 one-hot enconding 수행
 # 5 --> 0 0 0 0 0 1 0 0 0 0
 # 1 --> 0 1 0 0 0 0 0 0 0 0
 y_train = keras.utils.to_categorical(y=y_train, num_classes = 10)
@@ -20,22 +19,20 @@ print("X값 데이타형", x_train.shape, x_test.shape)
 
 start = time.time() # 시작 시간 저장
 
-#모델생성
+#모델 구조(계층) 생성
 model = keras.Sequential()
 model.add(keras.layers.Dense(32, activation="sigmoid", input_shape=(28*28,)))
 model.add(keras.layers.Dense(32, activation="sigmoid"))
 model.add(keras.layers.Dense(10, activation="sigmoid"))
 
-#모델컴파일
+#모델 컴파일
 model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.1), loss="categorical_crossentropy", metrics=['accuracy'])
 model.summary()
 
-#모델훈련
+#모델 훈련
 model.fit(x=x_train, y=y_train, batch_size=128, epochs=50, validation_data=(x_test, y_test))
 
 #모델 평가
 model.evaluate(x_test, y_test )
 
 print("Running Time :", round(time.time() - start, 2),"(Sec.)")  # 현재시각 - 시작시간 = 실행 시간
-
-
