1054775da62bacb0fe4a922bb0a1ad85f874a680
Moon-Kee Bahk(박문기, 朴文基) <30949880+mkbahk@users.noreply.github.com>
2021-03-10 14:59:41 +0900

    Update mnist-cnn-keras-seq+functional-ipu-pipeline-dist.py

--- mnist/mnist-cnn-keras-seq+functional-ipu-pipeline-dist.py
+++ mnist/mnist-cnn-keras-seq+functional-ipu-pipeline-dist.py
@@ -109,12 +109,12 @@ if __name__ == '__main__':
     strategy = ipu.ipu_strategy.IPUStrategy()
     with strategy.scope():
         # sequential model 훈련하기
-        print("\n\nTraining a Sequential MNIST Model.")
-        start = time.time()
-        train_model(sequential_model_fn())
-        print("Running Time :", round(time.time() - start, 2),"(Sec.)")
-
-        # funcaitonal model 훈련하기
+        #print("\n\nTraining a Sequential MNIST Model.")
+        #start = time.time()
+        #train_model(sequential_model_fn())
+        #print("Running Time :", round(time.time() - start, 2),"(Sec.)")
+        
+        #funcaitonal model 훈련하기
         print("\n\nTraining a Function MNIST Model.")
         start = time.time()
         train_model(functional_model_fn())
